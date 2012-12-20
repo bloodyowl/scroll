7c982c24374d3741344947ae9015869950ad5544
mlbli <hello@mlb.li>
2012-12-20 23:13:27 +0100

    Initialize

---
+++ scroll.js
@@ -0,0 +1,47 @@
+;(function(){
+  
+  var requestAnimationFrame = (function(){
+    return window.requestAnimationFrame
+        || window.webkitRequestAnimationFrame
+        || window.mozRequestAnimationFrame 
+        || window.oRequestAnimationFrame
+        || window.msRequestAnimationFrame
+        || function(callback){ window.setTimeout(callback, 1000/60)}
+  })()
+  , yOffset = "pageYOffset" in window
+  
+  function absoluteOffset(el){
+    var value = el.offsetTop
+    while(el && (el = el.offsetParent)) value += el.offsetTop
+    return value
+  }
+  
+  function scroll(to, duration) {
+    var origin = yOffset ? window.pageYOffset : document.documentElement.scrollTop
+      , start = +new Date()
+      , end = start + (duration || 500)
+      , direction
+    
+    if(typeof to == "object" && "nodeName" in to) to = absoluteOffset(to)
+    to = document.height < (to + window.innerHeight) ? document.height - window.innerHeight : to
+      
+    direction = to - origin < 0 ? true : false
+     
+    function handler(){
+      var now = +new Date()
+      if (now <= end) {
+        if(direction) scrollTo(0, origin - (now - start) / (end - start) * origin + to)
+        else scrollTo(0, origin + (now - start) / (end - start) * (to - origin))
+        requestAnimationFrame(handler)
+      } else {
+        scrollTo(0, to)
+      }
+    }
+
+    requestAnimationFrame(handler)
+  }
+  
+  document.scrollTo = scroll
+
+})()
+
